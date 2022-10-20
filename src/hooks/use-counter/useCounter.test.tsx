c562ba08bfbaf367f658e0854f54c3f9319ba7b3
Jeff Liu <49432090+JeffLiusGitHub@users.noreply.github.com>
2022-10-20 16:29:03 +0930

    add file

---
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -0,0 +1,63 @@
+import { renderHook, act, render } from '@testing-library/react'
+import { useCounter } from './useCounter'
+
+describe('useCounter', () => {
+  test('should render initial count', () => {
+    const { result } = renderHook(useCounter)
+    expect(result.current.count).toBe(0)
+  })
+  test('should accept and render the same initial count', () => {
+    const { result } = renderHook(useCounter, {
+      initialProps: {
+        initialCount: 10,
+      },
+    })
+    expect(result.current.count).toBe(10)
+  })
+  test('should increment count', () => {
+    const { result } = renderHook(useCounter)
+    act(() => {
+      result.current.increment()
+    })
+
+    expect(result.current.count).toBe(1)
+    act(() => {
+      result.current.decrement()
+    })
+    expect(result.current.count).toBe(0)
+  })
+  test('should decrement count', () => {
+    const { result } = renderHook(useCounter)
+
+    act(() => {
+      result.current.decrement()
+    })
+    expect(result.current.count).toBe(-1)
+  })
+})
+
+// describe('useCounter', () => {
+//   test('should render the initial count', () => {
+//     const { result } = renderHook(useCounter)
+//     expect(result.current.count).toBe(0)
+//   })
+
+//   test('should accept and render the same initial count', () => {
+//     const { result } = renderHook(useCounter, {
+//       initialProps: { initialCount: 10 },
+//     })
+//     expect(result.current.count).toBe(10)
+//   })
+
+//   test('should increment the count', () => {
+//     const { result } = renderHook(useCounter)
+//     act(() => result.current.increment())
+//     expect(result.current.count).toBe(1)
+//   })
+
+//   test('should decrement the count', () => {
+//     const { result } = renderHook(useCounter)
+//     act(() => result.current.decrement())
+//     expect(result.current.count).toBe(-1)
+//   })
+// })
